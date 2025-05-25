b90378a756c0d96b4fea6fae16a6af735b1a1eb4
akhilrao-v <raoakhil1999@gmail.com>
2025-05-25 08:50:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+import ScoreCard from "./components/scorecard/ScoreCard";
+import ScoreCardOperations from "./components/scoreCardOperations/ScoreCardOperations";
+function App() {
+  let [score, scoreFun] = useState(0);
+  console.log("app component rendering", score);
+
+  // function incrementCounter() {
+  //   console.log("I am incrementing");
+  //   score = score + 1;
+  //   scoreFun(score);
+  //   console.log("updated score", score);
+  // }
+
+  function incrementCounter(value) {
+    console.log("I am incrementing");
+    score = score + value;
+    scoreFun(score);
+    console.log("updated score", score);
+  }
+
+  // function incrementBy5Counter() {
+  //   console.log("I am incrementing");
+  //   score = score + 5;
+  //   scoreFun(score);
+  //   console.log("updated score", score);
+  // }
+
+  // function normalTestFun() {
+  //   console.log("hi hi hi");
+  // }
+
+  //useCall, useMemo, React.memo
+  return (
+    <div>
+      <ScoreCard score={score} />
+      <ScoreCardOperations
+        incrementScore={incrementCounter}
+        // incrementScoreBy5={incrementBy5Counter}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/message/Message.js
@@ -0,0 +1,6 @@
+import "./Message.css";
+function Message() {
+  console.log("message component");
+  return <h4 className="message">Counter-application React JS State management</h4>;
+}
+export default Message;
